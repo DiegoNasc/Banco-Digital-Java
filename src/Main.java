1a942019122d2eabe80534968d6235eec670b38f
DiegoNasc <diegod_dsn@hotmail.com>
2022-11-10 22:26:37 -0300

    Implementação de código

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cliente diego = new Cliente();
+        diego.setNome("Diego");
+
+        Conta cc = new ContaCorrente(diego);
+        Conta poupanca = new ContaPoupanca(diego);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+    }
+}
